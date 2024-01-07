e42ed4dcc8d0afc5b2a6d51cc6f40b30fe377ec9
Sam Seabourn <sam.seabourn@gmail.com>
2024-01-07 15:23:20 +1100

    chore: update svetle.config

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.argv.includes('dev');
 
+console.log(process.env.BASE_PATH)
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({}),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH
+			base: dev ? '' : '/chrome-extension-studio'
 		}
 	}
 };
