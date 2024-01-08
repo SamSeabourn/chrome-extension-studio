93c38d4b0ad7af20c0e4972426fa68427d7d3b2b
Sam Seabourn <sam.seabourn@gmail.com>
2024-01-08 20:35:39 +1100

    Updates

--- _app/immutable/chunks/paths.-IbnqBga.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1o21cbi?.base??"/chrome-extension-studio",e=globalThis.__sveltekit_1o21cbi?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/paths.H8whXvaE.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_e4ad01?.base??"/chrome-extension-studio",e=globalThis.__sveltekit_e4ad01?.assets??s;export{e as a,s as b};
