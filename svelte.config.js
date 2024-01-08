a4e9ba6c2954dae2370d483793420ad4833d50ba
Sam Seabourn <sam.seabourn@gmail.com>
2024-01-08 21:12:22 +1100

    chore: remove comment

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-console.log('HERE', process.env.NODE_ENV);
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
